1e1fe383ae8ed5fc7ee81d1b9cf204fcb8095834
Jaime RGP <jaime.rogue@gmail.com>
2017-11-10 17:11:58 +0100

    Add setup.py and provide executable CLI

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+import io
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(os.path.join(here, filename), encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+
+setup(
+    name='supporting',
+    version='0.0.1',
+    url='https://github.com/insilichem/supporting',
+    author='Jaime Rodríguez-Guerra',
+    author_email='jaime.rogue@gmail.com',
+    description=long_description,
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Chemistry',
+    ],
+    install_requires=['cclib==1.4.1'],
+    entry_points='''
+        [console_scripts]
+        qmview=supporting.cli:main
+        '''
+)
---
+++ supporting/cli.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+################################################
+#        QMVIEW: Preview QM calculations       #
+# -------------------------------------------- #
+# By Jaime RGP <jaime.rogue@gmail.com> @ 2016  #
+################################################
+
+"""
+Preview a Gaussian output file in terminal
+
+Requires:
+    - python 2.7
+    - pymol 1.7.4.0
+    - cclib 1.4.1
+    - libcaca 0.99 (other versions might work too)
+"""
+
+from __future__ import division, print_function
+import argparse
+import os
+import shutil
+import subprocess
+import sys
+from cStringIO import StringIO
+from supporting import generate, HAS_PYMOL
+
+
+def run(path, xyz=False, preview=True, width=300):
+    fd = StringIO()
+    report = generate(path, output_filehandler=fd, image=preview)
+    fd.seek(0)
+    if xyz:
+        print(fd.read())
+    else:
+        print(''.join(fd.readlines()[:16]))
+    if HAS_PYMOL and preview:
+        imgpath = path + '.png'
+        try:
+            subprocess.call(['img2txt', '--format', 'utf8', '--width', str(width), imgpath])
+        except OSError:
+            pass
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Preview a Gaussian output file')
+    parser.add_argument('paths', metavar='PATH', type=str, nargs='+',
+                        help='One or more paths to *.out, *.qfi files')
+    parser.add_argument('--xyz', action='store_true', default=False,
+                        help='Print XYZ coordinates')
+    parser.add_argument('--nopreview', action='store_true', default=False,
+                        help='Disable terminal preview')
+    parser.add_argument('-w', '--width', type=int, default=300,
+                        help='Output width of terminal preview')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    for path in args.paths:
+        run(path, xyz=args.xyz, preview=not args.nopreview, width=args.width)
+
+
+if __name__ == '__main__':
+    main()
