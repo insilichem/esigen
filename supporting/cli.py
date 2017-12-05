0aad9dcdc4baefe8475a9faebd7b44919265fa12
Jaime RGP <jaime.rogue@gmail.com>
2017-12-05 18:33:18 +0100

    cli: simplify logic

--- supporting/cli.py
+++ supporting/cli.py
@@ -23,42 +23,23 @@ import os
 import shutil
 import subprocess
 import sys
-try:
-    from cStringIO import StringIO
-except ImportError:  # Python 3?
-    from io import StringIO
-from supporting.core import generate
+from supporting.io import GaussianInputFile
 
 
-def run(path, xyz=False, preview=True, width=300, show_NAs=False):
-    fd = StringIO()
-    report = generate(path, output_filehandler=fd, image=preview,
-                      show_NAs=show_NAs)
-    fd.seek(0)
-    if xyz:
-        print(fd.read())
-    else:
-        lines = fd.readlines()
-        print(''.join(lines[:lines.index('__Molecular Geometry in Cartesian Coordinates__\n')]))
-
-    if preview:
-        imgpath = path + '.png'
-        try:
-            subprocess.call(['img2txt', '--format', 'utf8', '--width', str(width), imgpath])
-        except OSError:
-            pass
+def run(path, template='default.md', show_NAs=False, preview=True):
+    g = GaussianInputFile(sys.argv[1])
+    g.parse()
+    return g.report(template=template, show_NAs=show_NAs, preview=preview)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Preview a Gaussian output file')
     parser.add_argument('paths', metavar='PATH', type=str, nargs='+',
                         help='One or more paths to *.out, *.qfi files')
-    parser.add_argument('--xyz', action='store_true', default=False,
-                        help='Print XYZ coordinates')
     parser.add_argument('--nopreview', action='store_true', default=False,
                         help='Disable terminal preview')
-    parser.add_argument('-w', '--width', type=int, default=300,
-                        help='Output width of terminal preview')
+    parser.add_argument('--template', type=str, default='default.md',
+                        help='Jinja template to render report')
     parser.add_argument('--show_NAs', action='store_true', default=False,
                         help='Whether to show rows for not available data')
     return parser.parse_args()
@@ -75,8 +56,8 @@ def main():
             '  conda install -c omnia -c egilliesix pymol libglu python=2.7', file=sys.stderr)
     args = parse_args()
     for path in args.paths:
-        run(path, xyz=args.xyz, preview=HAS_PYMOL and not args.nopreview, width=args.width,
-            show_NAs=args.show_NAs)
+        print(run(path, args.template, preview=HAS_PYMOL and not args.nopreview,
+              show_NAs=args.show_NAs))
 
 
 if __name__ == '__main__':
