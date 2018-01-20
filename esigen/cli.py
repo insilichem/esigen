93a0f5fd5c43d4b45839863dc3fd494f46fb6c46
Jaime RGP <jaime.rogue@gmail.com>
2018-01-20 16:38:18 +0100

    Docs: improve CLI usage

--- esigen/cli.py
+++ esigen/cli.py
@@ -24,6 +24,7 @@ import shutil
 import subprocess
 import sys
 from esigen import ESIgenReport
+from esigen.core import BUILTIN_TEMPLATES
 
 
 def run(path, template='default.md', missing=None, preview=True, reporter=ESIgenReport):
@@ -38,8 +39,11 @@ def parse_args():
     parser.add_argument('paths', metavar='PATH', type=str, nargs='+',
                         help='One or more paths to *.out, *.qfi files')
     parser.add_argument('--nopreview', action='store_true', default=False,
-                        help='Disable terminal preview')
+                        help='Disable image preview (requires PyMol and libcaca)')
     parser.add_argument('--template', type=str, default='default.md',
+                        help='Jinja template to render report (builtin: {}). '
+                             'Check the documentation to learn more on how to '
+                             'build your own templates.'.format(', '.join(BUILTIN_TEMPLATES)))
     parser.add_argument('--missing', type=str, default='N/A',
                         help='Value to show if a requested field was not found in the '
                              'provided file(s). By default, do not show them.')
@@ -47,15 +51,17 @@ def parse_args():
 
 
 def main():
+    args = parse_args()
     try:
         import pymol
         pymol.finish_launching(['pymol', '-qc'])
         HAS_PYMOL = True
     except ImportError:
         HAS_PYMOL = False
-        print('Install PyMOL to render images! With conda, use:\n'
-            '  conda install -c omnia -c egilliesix pymol libglu python=2.7', file=sys.stderr)
-    args = parse_args()
+        if not args.nopreview:
+            print('Install PyMOL to render images! With conda, use:\n'
+                  '  conda install -c omnia -c egilliesix pymol libglu python=2.7',
+                  file=sys.stderr)
     for path in args.paths:
         print(run(path, args.template, preview=HAS_PYMOL and not args.nopreview,
                   missing=args.missing))
